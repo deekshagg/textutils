d59cc42e2be72fd6aa8cfd9d8050b8fed8907e6f
Diksha Aggarwal <agarwal.deeksha498@gmail.com>
2022-02-18 00:28:40 +0530

    hooks understood lowercase + more utilites added

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,24 +1,38 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import propTypes from 'prop-types'
 
 
 export default function TextForm(props) {
-    const handleUpperCaseClick = ()=>{
+    const handleUpperCaseClick = () => {
         let newText = text.toUpperCase();
         setText(newText);
     }
-    const handleOnChange = (event)=>{
+    const handleLowerCaseClick = () => {
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
+    const handleOnChange = (event) => {
         setText(event.target.value);
     }
     const [text, setText] = useState('Enter text here');
     return (
-        <div>
-            <div className="mb-3">
-                <h1>{props.heading}</h1>
-                <textarea className="form-control" value = {text} onChange = {handleOnChange} id="TextArea" rows="8"></textarea>
+        <>
+            <div className='container'>
+                <div className="mb-3">
+                    <h1>{props.heading}</h1>
+                    <textarea className="form-control" value={text} onChange={handleOnChange} id="TextArea" rows="8"></textarea>
+                </div>
+                <button className="btn btn-primary mx-3" onClick={handleUpperCaseClick}>Convert to Uppercase</button>
+                <button className="btn btn-primary" onClick={handleLowerCaseClick}>Convert to Lowercase</button>
             </div>
-            <button className="btn btn-primary" onClick={handleUpperCaseClick}>Convert to Uppercase</button>
-        </div>
+            <div className="container my-3">
+                <h1>Your text summary</h1>
+                <p>{text.split(" ").length} words and {text.length} characters</p>
+                <p>{0.008 * text.split(" ").length} Minutes read</p>
+                <h3>Preview</h3>
+                <p>{text}</p>
+            </div>
+        </>
     )
 }
 TextForm.propTypes = {
